4fab3cca3edabb880b1ca5e9645c3bbb77ee1e1f
Donghyeon Lee <earthicko@gmail.com>
2023-06-11 11:32:00 +0100

    Docs: install script

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-sudo apt-get update -y | sudo apt-get upgrade -y
+sudo apt update -y | sudo apt upgrade -y
 
-sudo apt-get install git libglib2.0-dev python3-pip -y
+sudo apt install python3-pip -y
 
-sudo pip3 install asyncio
 sudo pip3 install asyncio_glib
 sudo pip3 install watchgod
+sudo pip3 install spidev
 
 sudo hostnamectl set-hostname SK-8845
 sudo sed -Ei 's/^127\.0\.1\.1.*$/127.0.1.1\tSK-8845/' /etc/hosts
